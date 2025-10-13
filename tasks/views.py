7a038db06dffb538b9cc85872cb233024c588171
AlexandrLutay <alex896201090091@hmail.com>
2025-10-13 14:55:24 +0300

    Добавлены views для взаимодействия с моделями

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class MyTaskListView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(assigned_to=self.request.user)
+
+class TaskCreateView(generics.CreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class TaskCompleteView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return self.retrieve(request, *args, **kwargs)
