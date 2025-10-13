897b22110d83c6a0288859a63f5d6ce91cf825ca
AlexandrLutay <alex896201090091@hmail.com>
2025-10-13 15:11:06 +0300

    Исправлена проблема с путями

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
 from .views import MyTaskListView, TaskCreateView, TaskCompleteView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('my-tasks/', MyTaskListView.as_view(), name='my-tasks'),
     path('create-task/', TaskCreateView.as_view(), name='create-task'),
     path('complete-task/<int:pk>/', TaskCompleteView.as_view(), name='complete-task'),
-
-    # JWT:
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
