e84a855f12a6975c44bcf08f8e828d5b35dd2dd8
AlexandrLutay <alex896201090091@hmail.com>
2025-10-13 19:06:05 +0300

    Добавлено дублирование регистрации моделей Task и TaskList в админке

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Task, TaskList
+
+admin.site.register(Task)
+admin.site.register(TaskList)
